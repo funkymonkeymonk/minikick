aff2e0a95651ab3e20438629e54583cd1b227d1f
Will Weaver <monkey@buildingbananas.com>
2015-11-28 20:48:53 +0000

    Writing happy path tests to flesh out CLI and forcing output

--- lib/minikick.rb
+++ lib/minikick.rb
@@ -2,7 +2,24 @@ require "thor"
 
 class Minikick < Thor
   desc "project PROJECT TARGET_AMOUNT", "Create a new project"
-  def project(project, target)
-    print("That's NUMBERWANG")
+  def project(project_name, target_amount)
+    puts("Added #{project_name} project with target of $#{target_amount}.")
+  end
+
+  desc "back USER_NAME PROJECT CREDIT_CARD_NUMBER BACKING_AMOUNT", "Back a project"
+  def back(user_name, project_name, credit_card_number, backing_amount)
+    puts("#{user_name} backed project #{project_name} for $#{backing_amount}.")
+  end
+
+  desc "list PROJECT", "List a projects backers and backed amounts"
+  def list(project_name)
+    puts("-- John backed for $50")
+    puts("-- Jane backed for $50")
+    puts("#{project_name} needs $400 more dollars to be successful.")
+  end
+
+  desc "backer USER_NAME", "Display all projects and amounts a backer has backed."
+  def backer(user_name)
+    print("-- Backed Awesome_Sauce for $50")
   end
 end
--- spec/minikick_spec.rb
+++ spec/minikick_spec.rb
@@ -16,14 +16,43 @@ end
 
 describe Minikick do
   describe "#project" do
-    let(:output) { capture(:stdout) { subject.project project_name, target_amount } }
+    let(:project_name) { "Awesome_Sauce" }
+    let(:target_amount) { 500 }
 
-    context "when project name and backing amount is valid" do
-      let(:project_name) { "Test_proj-ect" }
-      let(:target_amount) { 1000.25 }
-      it 'returns a success message' do
-        expect(output).to eq("That's NUMBERWANG")
-      end
+    context "when all data is valid" do
+      let(:result) { "Added #{project_name} project with target of $#{target_amount}.\n" }
+      specify { expect { subject.project project_name, target_amount }.to output(result).to_stdout }
     end
+  end
+
+  describe "#back" do
+    let(:user_name) { "John" }
+    let(:project_name) { "Awesome_Sauce" }
+    let(:credit_card_number) { "4111111111111111" }
+    let(:backing_amount) { 50 }
+
+    context "when all data is valid" do
+      let(:result) { "#{user_name} backed project #{project_name} for $#{backing_amount}.\n" }
+      specify { expect { subject.back user_name, project_name, credit_card_number, backing_amount }.to output(result).to_stdout }
+    end
+  end
+
+  describe "#list" do
+    let(:project_name) { "Awesome_Sauce" }
+
+    context "when project is not completely funded" do
+      let(:first_backer) { /-- John backed for $50/ }
+      let(:second_backer) { /-- Jane backed for $50/ }
+      let(:remaining) { "#{project_name} needs $400 more dollars to be successful.\n" }
+      # specify { expect { subject.list project_name}.to output(first_backer).to_stdout }
+      # specify { expect { subject.list project_name}.to output(second_backer).to_stdout }
+      specify { expect { subject.list project_name}.to output(end_with(remaining)).to_stdout }
+    end
+  end
+  #
+  describe "#backer" do
+    let(:user_name) { "John" }
+    let(:backed) { "-- Backed Awesome_Sauce for $50" }
+    specify { expect { subject.backer user_name}.to output(backed).to_stdout }
   end
 end
