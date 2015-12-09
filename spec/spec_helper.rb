e438c4c3ab3d8d7f6b80a6a340164847b2a55039
Will Weaver <monkey@buildingbananas.com>
2015-12-09 16:19:25 +0000

    Supress STDOUT and STDERR messages during test execution

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,20 @@ require 'dm-rspec'
 SimpleCov.start
 
 RSpec.configure do |config|
+  # Redirect STDOUT and STDERR to /dev/NULL as per:
+  # http://stackoverflow.com/questions/15430551/suppress-console-output-during-rspec-tests
+  original_stderr = $stderr
+  original_stdout = $stdout
+  config.before(:all) do
+    # Redirect stderr and stdout
+    $stderr = File.open(File::NULL, "w")
+    $stdout = File.open(File::NULL, "w")
+  end
+  config.after(:all) do
+    $stderr = original_stderr
+    $stdout = original_stdout
+  end
+
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
